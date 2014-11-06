94d149bd0ed6de0dca2a0c7c5cb7917b5ab3e890
Phillip Mienk <mienkphi@gmail.com>
2014-11-05 23:58:31 -0800

    Initial seed from libbitcoin extracted code.

---
+++ include/bitcoin/node.hpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2011-2014 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin-protocol.
+ *
+ * libbitcoin-protocol is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LIBBITCOIN_NODE_NODE_HPP
+#define LIBBITCOIN_NODE_NODE_HPP
+
+#include <bitcoin/bitcoin.hpp>
+#include <bitcoin/node/define.hpp>
+#include <bitcoin/node/getx_responder.pb.h>
+#include <bitcoin/node/poller.hpp>
+#include <bitcoin/node/session.hpp>
+#include <bitcoin/node/transaction_indexer.hpp>
+
+#endif
+
