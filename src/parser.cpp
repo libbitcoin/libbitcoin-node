78a06799f6269ebe6c0cc49ec6a4038587c753bf
evoskuil <eric@voskuil.org>
2016-02-02 02:47:21 -0800

    Remove dead code, fix config path break.

--- src/parser.cpp
+++ src/parser.cpp
@@ -100,7 +100,7 @@ options_metadata parser::load_environment()
         // This composes with the cmdline options and inits to system path.
         BN_CONFIG_VARIABLE,
         value<path>(&settings.file)->composing()
-            ->default_value(default_config_path()),
+            ->default_value(config_default_path()),
         "The path to the configuration settings file."
     );
 
