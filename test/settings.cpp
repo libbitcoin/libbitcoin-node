bc12ada51e73d5f3fcc5e676385f6a99efa2749c
evoskuil <eric@voskuil.org>
2016-04-30 01:02:43 -0700

    Add settings unit tests.

---
+++ test/settings.cpp
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2011-2015 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin-node.
+ *
+ * libbitcoin-node is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <boost/test/unit_test.hpp>
+#include <bitcoin/node.hpp>
+
+using namespace bc;
+
+BOOST_AUTO_TEST_SUITE(settings_tests)
+
+// constructors
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(settings__construct__default_context__expected)
+{
+    node::settings configuration;
+    BOOST_REQUIRE(configuration.peers.empty());
+    BOOST_REQUIRE_EQUAL(configuration.download_connections, 8u);
+    BOOST_REQUIRE_EQUAL(configuration.block_timeout_seconds, 5u);
+}
+
+BOOST_AUTO_TEST_CASE(settings__construct__none_context__expected)
+{
+    node::settings configuration(bc::settings::none);
+    BOOST_REQUIRE(configuration.peers.empty());
+    BOOST_REQUIRE_EQUAL(configuration.download_connections, 8u);
+    BOOST_REQUIRE_EQUAL(configuration.block_timeout_seconds, 5u);
+}
+
+BOOST_AUTO_TEST_CASE(settings__construct__mainnet_context__expected)
+{
+    node::settings configuration(bc::settings::mainnet);
+    BOOST_REQUIRE(configuration.peers.empty());
+    BOOST_REQUIRE_EQUAL(configuration.download_connections, 8u);
+    BOOST_REQUIRE_EQUAL(configuration.block_timeout_seconds, 5u);
+}
+
+BOOST_AUTO_TEST_CASE(settings__construct__testnet_context__expected)
+{
+    node::settings configuration(bc::settings::testnet);
+    BOOST_REQUIRE(configuration.peers.empty());
+    BOOST_REQUIRE_EQUAL(configuration.download_connections, 8u);
+    BOOST_REQUIRE_EQUAL(configuration.block_timeout_seconds, 5u);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
