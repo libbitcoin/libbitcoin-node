b0ee20497dc4b42d54a97e0d1f5f14d1d67caa95
evoskuil <eric@voskuil.org>
2016-09-22 21:33:31 -0700

    Add node::session<> to capture p2pnode reference in intermediate base.

---
+++ include/bitcoin/node/sessions/session.hpp
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2011-2015 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LIBBITCOIN_NODE_SESSION_HPP
+#define LIBBITCOIN_NODE_SESSION_HPP
+
+#include <bitcoin/network.hpp>
+#include <bitcoin/node/define.hpp>
+
+namespace libbitcoin {
+namespace node {
+
+class p2p_node;
+
+/// Intermediate session base class template.
+/// This avoids having to make network::session into a tempalte.
+template <class Session>
+class BCN_API session
+  : public Session
+{
+public:
+    session(p2p_node& network, bool notify_on_connect)
+      : Session(network, notify_on_connect), node_network_(network)
+    {
+    }
+
+protected:
+
+    // This is thread safe.
+    p2p_node& node_network_;
+};
+
+} // namespace node
+} // namespace libbitcoin
+
+#endif
+
