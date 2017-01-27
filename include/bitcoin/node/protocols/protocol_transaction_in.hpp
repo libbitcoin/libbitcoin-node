79e14ed0fa7cc3d854310059a877614f75634e00
evoskuil <eric@voskuil.org>
2017-01-27 15:16:38 -0800

    Remove disabled code comments.

--- include/bitcoin/node/protocols/protocol_transaction_in.hpp
+++ include/bitcoin/node/protocols/protocol_transaction_in.hpp
@@ -53,10 +53,6 @@ private:
     void handle_store_transaction(const code& ec,
         transaction_const_ptr message);
 
-    ////bool handle_reorganized(code ec, size_t fork_height,
-    ////    block_const_ptr_list_const_ptr incoming,
-    ////    block_const_ptr_list_const_ptr outgoing);
-
     void handle_stop(const code&);
 
     blockchain::safe_chain& chain_;
