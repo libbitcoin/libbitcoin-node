3f724c2f3b91fa172b4d0db18c21305311ff1a85
Phillip Mienk <mienkphi@gmail.com>
2017-02-08 20:46:24 -0800

    Update copyright/license headers.

--- src/full_node.cpp
+++ src/full_node.cpp
@@ -1,21 +1,20 @@
 /**
- * Copyright (c) 2011-2015 libbitcoin developers (see AUTHORS)
+ * Copyright (c) 2011-2017 libbitcoin developers (see AUTHORS)
  *
- * This file is part of libbitcoin-node.
+ * This file is part of libbitcoin.
  *
- * libbitcoin-node is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License with
- * additional permissions to the one published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version. For more information see LICENSE.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <bitcoin/node/full_node.hpp>
 
@@ -233,7 +232,7 @@ network::session_outbound::ptr full_node::attach_outbound_session()
 
 session_header_sync::ptr full_node::attach_header_sync_session()
 {
-    return attach<session_header_sync>(hashes_, chain_, 
+    return attach<session_header_sync>(hashes_, chain_,
         chain_.chain_settings().checkpoints);
 }
 
--- test/utility.cpp
+++ test/utility.cpp
@@ -1,21 +1,20 @@
 /**
- * Copyright (c) 2011-2015 libbitcoin developers (see AUTHORS)
+ * Copyright (c) 2011-2017 libbitcoin developers (see AUTHORS)
  *
- * This file is part of libbitcoin-node.
+ * This file is part of libbitcoin.
  *
- * libbitcoin-node is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License with
- * additional permissions to the one published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version. For more information see LICENSE.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "utility.hpp"
 
@@ -53,7 +52,7 @@ message::headers::ptr message_factory(size_t count)
 }
 
 // Create a headers message of specified size, using specified previous hash.
-message::headers::ptr message_factory(size_t count, 
+message::headers::ptr message_factory(size_t count,
     const hash_digest& previous)
 {
     auto previous_hash = previous;
@@ -175,7 +174,7 @@ bool blockchain_fixture::get_last_height(size_t& out_height) const
     return false;
 }
 
-bool blockchain_fixture::get_output(chain::output& out_output, 
+bool blockchain_fixture::get_output(chain::output& out_output,
     size_t& out_height, size_t& out_position,
     const chain::output_point& outpoint) const
 {
