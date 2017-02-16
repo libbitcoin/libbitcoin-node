5ca980da9a86d2ea1c7d211310e36cc50529b1da
evoskuil <eric@voskuil.org>
2017-02-16 04:55:03 -0800

    Update version3 branch installer to build from version3 branches. Fix version3 branch name typo.

--- install.sh
+++ install.sh
@@ -700,12 +700,12 @@ build_all()
 {
     build_from_tarball_boost $BOOST_URL $BOOST_ARCHIVE bzip2 . $PARALLEL "$BUILD_BOOST" "${BOOST_OPTIONS[@]}"
     build_from_github libbitcoin secp256k1 version4 $PARALLEL ${SECP256K1_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin master $PARALLEL ${BITCOIN_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-consensus version2 $PARALLEL ${BITCOIN_CONSENSUS_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-database master $PARALLEL ${BITCOIN_DATABASE_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-blockchain master $PARALLEL ${BITCOIN_BLOCKCHAIN_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-network master $PARALLEL ${BITCOIN_NETWORK_OPTIONS[@]} "$@"
-    build_from_travis libbitcoin libbitcoin-node master $PARALLEL ${BITCOIN_NODE_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin version3 $PARALLEL ${BITCOIN_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin-consensus version3 $PARALLEL ${BITCOIN_CONSENSUS_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin-database version3 $PARALLEL ${BITCOIN_DATABASE_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin-blockchain version3 $PARALLEL ${BITCOIN_BLOCKCHAIN_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin-network version3 $PARALLEL ${BITCOIN_NETWORK_OPTIONS[@]} "$@"
+    build_from_travis libbitcoin libbitcoin-node version3 $PARALLEL ${BITCOIN_NODE_OPTIONS[@]} "$@"
 }
 
 
