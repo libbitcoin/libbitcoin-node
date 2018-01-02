b800888573ad92cc6045ad3ad0bd8cbc0e5f6988
evoskuil <eric@voskuil.org>
2018-01-01 19:52:23 -0800

    Revise and generate msvc build-all commands.

--- builds/msvc/build/buildall.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-ECHO.
-ECHO Downloading Libbitcoin Node dependencies from NuGet
-CALL nuget.exe install ..\vs2013\libbitcoin-node\packages.config
-CALL nuget.exe install ..\vs2013\libbitcoin-node-test\packages.config
-ECHO.
-CALL buildbase.bat ..\vs2013\libbitcoin-node.sln 12
-ECHO.
-PAUSE
