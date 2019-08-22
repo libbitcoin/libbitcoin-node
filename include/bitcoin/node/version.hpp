7df8d719d13c6a13cfc64ab735548b27ba76cc93
Neill Miller <neillm@thecodefactory.org>
2019-08-21 21:15:10 -0400

    Regenerate build artifacts.

--- include/bitcoin/node/version.hpp
+++ include/bitcoin/node/version.hpp
@@ -12,9 +12,9 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define LIBBITCOIN_NODE_VERSION "3.4.0"
+#define LIBBITCOIN_NODE_VERSION "3.6.0"
 #define LIBBITCOIN_NODE_MAJOR_VERSION 3
-#define LIBBITCOIN_NODE_MINOR_VERSION 4
+#define LIBBITCOIN_NODE_MINOR_VERSION 6
 #define LIBBITCOIN_NODE_PATCH_VERSION 0
 
 #endif
