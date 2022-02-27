8758d3142c08a3bfcc4a7388ef0e52e0907af6a3
evoskuil <eric@voskuil.org>
2022-02-26 22:23:24 -0800

    Regenerate artifacts, add vs2019/2022.

--- builds/msvc/build/nuget_all.bat
+++ builds/msvc/build/nuget_all.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
+ECHO Downloading libbitcoin vs2022 dependencies from NuGet
+CALL nuget.exe install ..\vs2022\libbitcoin-node\packages.config
+CALL nuget.exe install ..\vs2022\libbitcoin-node-test\packages.config
+ECHO.
+ECHO Downloading libbitcoin vs2019 dependencies from NuGet
+CALL nuget.exe install ..\vs2019\libbitcoin-node\packages.config
+CALL nuget.exe install ..\vs2019\libbitcoin-node-test\packages.config
+ECHO.
 ECHO Downloading libbitcoin vs2017 dependencies from NuGet
 CALL nuget.exe install ..\vs2017\libbitcoin-node\packages.config
 CALL nuget.exe install ..\vs2017\libbitcoin-node-test\packages.config
-ECHO.
-ECHO Downloading libbitcoin vs2015 dependencies from NuGet
-CALL nuget.exe install ..\vs2015\libbitcoin-node\packages.config
-CALL nuget.exe install ..\vs2015\libbitcoin-node-test\packages.config
-ECHO.
-ECHO Downloading libbitcoin vs2013 dependencies from NuGet
-CALL nuget.exe install ..\vs2013\libbitcoin-node\packages.config
-CALL nuget.exe install ..\vs2013\libbitcoin-node-test\packages.config
