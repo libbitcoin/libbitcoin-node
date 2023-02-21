745b0124fa6a0c0fa8baf1b90ef8766e63bb5a47
evoskuil <eric@voskuil.org>
2023-02-21 02:56:36 -0800

    Conditionally exclude msvc only setting from test.

--- test/configuration.cpp
+++ test/configuration.cpp
@@ -33,7 +33,9 @@ BOOST_AUTO_TEST_CASE(configuration__construct1__none_context__expected)
     BOOST_REQUIRE_EQUAL(instance.log.verbose, false);
     BOOST_REQUIRE_EQUAL(instance.log.maximum_size, 1'000'000_u32);
     BOOST_REQUIRE_EQUAL(instance.log.path, "");
+#if defined(HAVE_MSC)
     BOOST_REQUIRE_EQUAL(instance.log.symbols, "");
+#endif
     BOOST_REQUIRE_EQUAL(instance.log.file1(), "bn_end.log");
     BOOST_REQUIRE_EQUAL(instance.log.file2(), "bn_begin.log");
 }
