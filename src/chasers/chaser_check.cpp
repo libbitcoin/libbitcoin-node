484ca812bdf9ca995a956341882ac048a2225beb
evoskuil <eric@voskuil.org>
2024-03-06 12:04:54 -0500

    Assert expected variant type.

--- src/chasers/chaser_check.cpp
+++ src/chasers/chaser_check.cpp
@@ -75,6 +75,7 @@ void chaser_check::handle_event(const code&, chase event_,
 {
     if (event_ == chase::header)
     {
+        BC_ASSERT(std::holds_alternative<chaser::height_t>(value));
         handle_header(std::get<height_t>(value));
     }
 }
