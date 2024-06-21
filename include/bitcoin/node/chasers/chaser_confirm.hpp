6218311f3e6c33f7b9b029c3f79c8e702fd7dcb4
evoskuil <eric@voskuil.org>
2024-06-21 15:46:57 -0400

    Use if !defined(UNDEFINED) vs. comments.

--- include/bitcoin/node/chasers/chaser_confirm.hpp
+++ include/bitcoin/node/chasers/chaser_confirm.hpp
@@ -47,7 +47,7 @@ protected:
         event_value value) NOEXCEPT;
 
     virtual void do_validated(height_t height) NOEXCEPT;
-#if defined(DISABLED)
+#if defined(UNDEFINED)
     virtual void do_organize(size_t height) NOEXCEPT;
     virtual bool enqueue_block(const database::header_link& link) NOEXCEPT;
     virtual void confirm_tx(const database::context& ctx,
@@ -58,7 +58,7 @@ protected:
         const database::header_link& link, size_t height)NOEXCEPT;
     virtual void confirm_block(const code& ec,
         const database::header_link& link, size_t height) NOEXCEPT;
-#endif // DISABLED
+#endif // UNDEFINED
 
 private:
     bool set_organized(const database::header_link& link,
